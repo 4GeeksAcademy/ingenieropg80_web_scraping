b1d4ea46f91389d83a833b798f45a937751abced
Pedro Gonzalez <ingenieropg80@gmail.com>
2025-06-24 20:02:40 +0000

    Ingenieropg80_hasta el 3

--- src/app.py
+++ src/app.py
@@ -5,4 +5,17 @@ import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from io import StringIO
+
+#COMO EXPLICO CARLOS TRAAJAR TABLAS CON PANDAS
+URL = "https://en.wikipedia.org/wiki/List_of_most-streamed_songs_on_Spotify"
+response= requests.get(URL)
+html = response.text
+tablas =pd.read_html(StringIO(html))
+df = tablas[0]
+
+print(df.head())
+
+df = df.dropnas(subset=["Song", "Artist"]) ?????????????????????????
 
