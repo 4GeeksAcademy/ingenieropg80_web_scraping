90c897a8947b06704296bcd6b7106ba4d1eaa503
Pedro Gonzalez <ingenieropg80@gmail.com>
2025-07-07 20:01:09 +0000

    ingenieropg80.webscraping.graf

--- src/app.py
+++ src/app.py
@@ -8,21 +8,5 @@ import seaborn as sns
 import pandas as pd
 from io import StringIO
 
-#COMO EXPLICO CARLOS TRAAJAR TABLAS CON PANDAS
-URL = "https://en.wikipedia.org/wiki/List_of_most-streamed_songs_on_Spotify"
-response= requests.get(URL)
-html = response.text
-tablas =pd.read_html(StringIO(html))
-df = tablas[0]
 
-df.columns = [col.strip() for col in df.columns]
-df.rename(columns={"Streams (billions)": "Streams"}, inplace=True)
-df['Streams'] = df['Streams'].astype(str)
-df = df[df['Streams'].str.contains(r'^\d+\.?\d*$', na=False)]
-df['Streams'] = df['Streams'].astype(float)
-df.dropna(subset=["Song", "Artist(s)", "Streams", "Release date"], inplace=True)
-
-print(df.head())
-
-#df = df.dropnas(subset=["Song", "Artist"]) ?????????????????????????
 
